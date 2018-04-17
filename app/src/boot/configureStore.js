ce8c090ee5fdee9af058f15287a703cea4a540a6
Jonas <grafjonas@web.de>
2018-04-17 11:20:18 +0200

    fix redux debugger setup

--- app/src/boot/configureStore.js
+++ app/src/boot/configureStore.js
@@ -1,21 +1,15 @@
 // @flow
 // import { AsyncStorage } from "react-native";
-import devTools from 'remote-redux-devtools';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import reducer from '../reducers';
 
 export default function configureStore(onCompletion: () => void): any {
-  const enhancer = compose(
-    applyMiddleware(thunk),
-    devTools({
-      name: 'parkspot',
-      realtime: true,
-    }),
-  );
+  const composeEnhancers = composeWithDevTools({ realtime: true, port: 5678 });
 
-  const store = createStore(reducer, enhancer);
+  const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
   persistStore(store, onCompletion);
 
   return store;
