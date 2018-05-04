feee09ccb13e30fc3c1445b24d7390f8c5e80bca
Can Kattwinkel <kattwinkel@w11k.de>
2018-05-04 19:25:17 +0200

    feat(ci): adds gitlab-ci, includes migration into the project

--- api/src/modules/database/database.provider.ts
+++ api/src/modules/database/database.provider.ts
@@ -7,15 +7,10 @@ export const databaseProviders = [
   {
     provide: DbConnectionToken,
     useFactory: async (config: PrkConfig) => {
-
-      /**
-       * Stop TypeORM CLI from running in prod by removing this information. Otherwise the
-       * cli is trying to import .ts sources (what breaks). Migrations are being run by antoher
-       * task anyway.. */
-      const {migrations, ...requiredDbCOnfig} = config.dbConfig;
-
       return await createConnection({
-        ...requiredDbCOnfig,
+        ...config.dbConfig,
+        migrations: config.runtimeConfiguration.environment === 'prod' ? ['dist/migration/**/*.js'] : [],
+        migrationsRun: config.runtimeConfiguration.environment === 'prod',
         entities: [
           __dirname + '/../**/*.entity{.ts,.js}',
         ],
