4528b426fd24a8acb1e6febe588d297d63334513
Sebastian Koch <sebastian@0py.de>
2018-05-09 19:40:06 +0200

    Removes native base card implementation

--- app/src/components/MapCard/index.js
+++ app/src/components/MapCard/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Animated, StyleSheet} from 'react-native';
-import {Container, Card, CardItem, Body, Text, Icon} from 'native-base';
+import {StyleSheet} from 'react-native';
+
 
 import styles from './styles';
 
@@ -17,95 +17,11 @@ class MapCard extends React.Component<Props, State> {
         super(props);
 
         this.state = {
-            expanded: false,
-            animationProgress: new Animated.Value(0),
-            renderHeight: 0,
         };
     };
 
-    onLayout = event => {
-        const {height} = event.nativeEvent.layout;
-        this.setState({renderHeight: height});
-    };
-
-    showCard = (ms = 1000) => {
-        Animated.timing(
-            this.state.animationProgress,
-            {
-                toValue: 1,
-                duration: ms,
-                useNativeDriver: true,
-            }
-        ).start();
-    };
-
-    hideCard = (ms = 1000) => {
-        Animated.timing(
-            this.state.animationProgress,
-            {
-                toValue: 0,
-                duration: ms,
-                useNativeDriver: true,
-            }
-        ).start();
-    };
-
-
-    cardWasPressed = () => {
-        const currentState = this.state.expanded;
-        this.setState({expanded: !currentState});
-
-        if (currentState) {
-            this.hideCard(300);
-        } else {
-            this.showCard(300);
-        }
-    };
-
     render() {
-
-        // do not render anything if no parkspot is selected
-        if (!this.props.parkspot) {
-            return null;
-        }
-
-        const cardTransformY = this.state.animationProgress.interpolate({
-            inputRange: [0, 1],
-            outputRange: [this.state.renderHeight * 0.75, 0],
-        });
-        
-        const cardStyle = StyleSheet.flatten([styles.cardItem, {transform: [{translateY: cardTransformY}]}]);
-
-        const description = "[id=" + this.props.parkspot.id + ", lat=" + this.props.parkspot.lat
-            + ", lng=" + this.props.parkspot.lng + "]";
-
-        return (
-            <Card style={styles.card} onLayout={this.onLayout} pointerEvents={this.state.expanded ? "auto" : "box-none"}>
-                <CardItem button onPress={() => {
-                    this.cardWasPressed();
-                }}
-                          style={cardStyle}>
-                    <Body>
-                    <Text style={styles.title}>
-                      Lorem Ipsum (id={this.props.parkspot.id})
-                    </Text>
-                    <Container style={styles.icons}>
-                        <Icon type='MaterialCommunityIcons' name='parking'
-                              style={this.props.parkspot.available ? styles.iconEnabled : styles.iconDisabled}/>
-                        <Icon type='FontAwesome' name='wheelchair-alt'
-                              style={this.props.parkspot.handicapped ? styles.iconEnabled : styles.iconDisabled}/>
-                        <Icon type='MaterialCommunityIcons' name='battery-charging-60'
-                              style={this.props.parkspot.electricCharger ? styles.iconEnabled : styles.iconDisabled}/>
-                    </Container>
-                    <Container style={styles.content}>
-                        <Text>
-                            {description}
-                        </Text>
-                    </Container>
-                    </Body>
-                </CardItem>
-            </Card>
-        );
+        return null;
     }
 }
 
--- app/src/components/MapCard/styles.js
+++ app/src/components/MapCard/styles.js
@@ -1,57 +1,7 @@
 import {StyleSheet} from 'react-native';
 
-
-const iconTemplate = {
-    width: 40,
-    height: 40,
-};
-
 const styles = StyleSheet.create({
-    card: {
-        position: 'absolute',
-        bottom: '0%',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'transparent',
-    },
-    cardItem: {
-        borderWidth: 1,
-        borderTopRightRadius: 22,
-        borderTopLeftRadius: 22,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.8,
-        shadowRadius: 4,
-        elevation: 1,
-        backgroundColor: 'white',
 
-        width: '100%',
-        height: '95%',
-        position: 'absolute',
-        bottom: '0%',
-        zIndex: 20,
-    },
-    title: {
-        color: '#777',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    icons: {
-        marginTop: 10,
-        flexDirection: 'row',
-    },
-    iconEnabled: {
-        ...iconTemplate,
-        color: '#000',
-    },
-    iconDisabled: {
-        ...iconTemplate,
-        color: '#aaa',
-    },
-    content: {
-        marginTop: 20,
-    },
 });
+
 export default styles;
