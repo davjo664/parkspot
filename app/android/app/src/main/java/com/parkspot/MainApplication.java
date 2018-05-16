9eade59f8a4e3e048c61fe2e0e85ca1e4accb9c1
Sebastian Koch <sebastian@0py.de>
2018-05-16 18:03:10 +0200

    Adds dependency on react-native-open-settings

--- app/android/app/src/main/java/com/parkspot/MainApplication.java
+++ app/android/app/src/main/java/com/parkspot/MainApplication.java
@@ -3,6 +3,7 @@ package com.parkspot;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.opensettings.OpenSettingsPackage;
 import com.wix.interactable.Interactable;
 import com.microsoft.codepush.react.CodePush;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OpenSettingsPackage(),
             new Interactable(),
               new CodePush("YSIjMqHJfj-DO333VXte5sDuLRhJS1GPuXPnM", MainApplication.this, BuildConfig.DEBUG),
               new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
