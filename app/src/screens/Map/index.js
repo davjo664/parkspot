53f4ff0770547d96ecf1c0c3cc258d3fd7b09917
Jonas <grafjonas@web.de>
2018-07-04 11:17:22 +0200

    setting via callback and animate

--- app/src/screens/Map/index.js
+++ app/src/screens/Map/index.js
@@ -135,16 +135,14 @@ class Map extends React.Component<Props, State> {
     });
   };
 
-  setSelectedParkspot = (parkspot: Object) => {
-    this.props.updateMapPosition({
-      latitude: Number(parkspot.lat),
-      longitude: Number(parkspot.lng),
-      latitudeDelta: 0.0005,
-      longitudeDelta: 0.005,
-    });
-    this.setState({
-      selectedParkspot: parkspot,
-    });
+  setSelectedLocation = (location: Object) => {
+    //supply this function to search via Navigator to call when location should be set
+    this.map.getMapRef().animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      longitudeDelta: 0.05,
+      latitudeDelta: 0.05,
+    }, 0);
   };
 
   startNavigation = (parkspot) => {
@@ -241,20 +239,31 @@ class Map extends React.Component<Props, State> {
     PermissionHelper.hasPermission('location', () => {
       this.props.updateLocation();
       //location is usually already set so no need to wait for that... if bugs check here
-      this.props.updateMapPosition(
-        {
-          latitude: this.props.userPosition.latitude,
-          longitude: this.props.userPosition.longitude,
-          longitudeDelta: 0.05,
-          latitudeDelta: 0.05,
-        }
-      );
+      this.map.getMapRef().animateToRegion({
+        latitude: this.props.userPosition.latitude,
+        longitude: this.props.userPosition.longitude,
+        longitudeDelta: 0.05,
+        latitudeDelta: 0.05,
+      }, 1000);
+      console.log(this.map)
+      // this.props.updateMapPosition(
+      //   {
+      //     latitude: this.props.userPosition.latitude,
+      //     longitude: this.props.userPosition.longitude,
+      //     longitudeDelta: 0.05,
+      //     latitudeDelta: 0.05,
+      //   }
+      // );
+
+
       this.state.showsUserLocation = true;
     }, true);
   };
 
   searchButtonWasPressed = () => {
-    this.props.navigation.navigate('Search');
+    this.props.navigation.navigate('Search',
+      {'setLocation': (location) => this.setSelectedLocation(location)}
+    );
     this.props.deleteClosestParkspots();
   };
 
@@ -642,7 +651,7 @@ class Map extends React.Component<Props, State> {
         <ClusteredMapView
           style={styles.map}
           showsUserLocation={this.state.showsUserLocation}
-          region={this.props.mapPosition}
+          initialRegion={this.props.mapPosition}
           onRegionChangeComplete={this.onRegionChangeComplete}
           showsMyLocationButton={false}
           showsPointsOfInterest={true}
