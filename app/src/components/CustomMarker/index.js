6fd207a01e8eee3e5b7a883d27f636650bbcfea4
Jonas <grafjonas@web.de>
2018-07-04 14:52:54 +0200

    fix if for iOS

--- app/src/components/CustomMarker/index.js
+++ app/src/components/CustomMarker/index.js
@@ -24,29 +24,31 @@ export default class CustomMarker extends Component {
     const image = this.props.isCluster ? require('../../../assets/icons/map/clusterPin.png') : (this.props.isSelected ? require('../../../assets/icons/map/selectedPin.png') : require('../../../assets/icons/map/markerPin.png'));
 
     // Android does not seem to like ImageBackground inside a marker...
-    if (Platform.OS === 'iOS') {
-      <Marker key={this.props.identifier}
-              coordinate={this.props.coordinate}
-              onPress={this.props.onPress}>
-        <ImageBackground style={[styles.pin, styles.pinShadow, {
-          width: this.props.isSelected ? 65 : 32,
-          height: this.props.isSelected ? 58 : 32,
-        }]}
-                         source={image}>
-          <Text style={styles.pinText}>{this.props.label}</Text>
-        </ImageBackground>
-      </Marker>
+    if (Platform.OS === 'ios') {
+      return (
+        <Marker key={this.props.identifier}
+          coordinate={this.props.coordinate}
+          onPress={this.props.onPress}>
+          <ImageBackground style={[styles.pin, styles.pinShadow, {
+            width: this.props.isSelected ? 65 : 32,
+            height: this.props.isSelected ? 58 : 32,
+          }]}
+            source={image}>
+            <Text style={styles.pinText}>{this.props.label}</Text>
+          </ImageBackground>
+        </Marker>
+      );
     } else {
       return (
         <Marker key={this.props.identifier}
-                coordinate={this.props.coordinate}
-                onPress={this.props.onPress}
-                image={image}
-                style={[styles.pin, styles.pinShadow, {
-                  width: this.props.isSelected ? 65 : 32,
-                  height: this.props.isSelected ? 58 : 32,
-                }]}
-                anchor={this.props.isSelected ? {x: 0.4, y: 0.7} : null}>
+          coordinate={this.props.coordinate}
+          onPress={this.props.onPress}
+          image={image}
+          style={[styles.pin, styles.pinShadow, {
+            width: this.props.isSelected ? 65 : 32,
+            height: this.props.isSelected ? 58 : 32,
+          }]}
+          anchor={this.props.isSelected ? {x: 0.4, y: 0.7} : null}>
           <Text style={[styles.pinText, {
             fontSize: this.props.isSelected ? 18 : 20,
             marginLeft: this.props.isSelected ? -14 : 3,
